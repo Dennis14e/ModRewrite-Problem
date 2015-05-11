b0f35f92f791360f3048f0e609344ea1d2f6a902
Dennis Neufeld <info@dennis14e.de>
2015-05-11 21:40:30 +0200

    PHP leicht verbessert

--- karten/places.php
+++ karten/places.php
@@ -1,11 +1,22 @@
-<?php /* Laedt Content-File */ ?>
-<?php $kategorie = $_GET['kategorie'] ?>
-<?php $name = $_GET['name'] ?>
-<?php include ("../content/places/$kategorie/$name") ?>
+<?php
+/* Laedt Content-File */
+$kategorie = $_GET['kategorie'];
+$name = $_GET['name'];
+
+$pfad = "../content/places/$kategorie/$name";
+if(!file_exists($pfad))
+{
+	header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+	header('Status: 404 Not Found');
+	exit;
+}
+
+include $pfad;
+?>
 
 <body>
 
-<?php echo $titel ?></h1>
+<?php echo $titel; ?></h1>
 
 </body>
 </html>
